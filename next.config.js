6c10ee7a5c922411d978b078377076856d0f2fe2
Cayla4224 <crodri0297@gmail.com>
2025-09-22 18:42:05 +0000

    Image Fix

--- next.config.js
+++ next.config.js
@@ -1,11 +1,18 @@
+// file: next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    // simplest allow-list for Next 14
-    domains: ["picsum.photos"]
-    // Or use remotePatterns if you need more control:
-    // remotePatterns: [{ protocol: "https", hostname: "picsum.photos" }]
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "picsum.photos",
+        port: "",
+        pathname: "/**"
+      }
+    ]
   },
   experimental: { serverActions: { allowedOrigins: ["*"] } }
 };
+
 module.exports = nextConfig;
+
